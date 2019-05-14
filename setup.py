2e773598f50305a76c3f78c0c1b2b3747357c622
Daniel Dersch <dhdersch@gmail.com>
2019-05-14 14:55:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name="tljh-pangeo",
-    entry_points={"tljh": ["pangeo = tljh_pangeo"]},
-    py_modules=["tljh_pangeo"],
+    name="tljh-r",
+    entry_points={"tljh": ["r = tljh_r"]},
+    py_modules=["tljh_r"],
 )
 
